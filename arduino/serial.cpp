b2d24b058efb45342e0e20622ed9017786f95544
Cinntax <dcinnamon@gmail.com>
2019-01-27 18:42:54 -0600

    Fixed issues with serial.cpp

--- arduino/serial.cpp
+++ arduino/serial.cpp
@@ -1,5 +1,36 @@
 #include "order.h"
 
+void wait_for_bytes(int num_bytes, unsigned long timeout){
+  unsigned long startTime = millis();
+  //Wait for incoming bytes or exit if timeout
+  while ((Serial.available() < num_bytes) && (millis() - startTime < timeout)){}
+}
+
+RobotOrderType read_order(){
+  wait_for_bytes(1,1000); // Wait for 1 byte with a timeout of 1s.
+  if(Serial.available() > 0)
+    return (RobotOrderType) Serial.read();
+  else
+	return RobotOrderType.NOOP;
+}
+
+//This is used when I want to get multiple bytes from the stream.
+void read_signed_bytes(int8_t* buffer, size_t n){
+  size_t i = 0;
+  int c;
+  while (i < n){
+    c = Serial.read();
+	if (c < 0) break;
+    *buffer++ = (int8_t) c;
+    i++;
+  }
+}
+
+void read_color(int8_t* buffer) {
+  wait_for_bytes(3, 100); // Wait for 2 bytes with a timeout of 100 ms
+  read_signed_bytes(buffer, 3);
+}
+
 //Blocking method to get the next command from master.
 RobotOrder get_command_from_serial(){
   RobotOrderType current_order = read_order();
@@ -16,37 +47,6 @@ RobotOrder get_command_from_serial(){
 	return_value.data3 = color[2];
   }  
   return return_value;  
-}
-
-RobotOrderType read_order(){
-  wait_for_bytes(1,1000); // Wait for 1 byte with a timeout of 1s.
-  if(Serial.available() > 0)
-    return (RobotOrderType) Serial.read();
-  else
-	return RobotOrderType.NOOP;
-}
-
-void wait_for_bytes(int num_bytes, unsigned long timeout){
-  unsigned long startTime = millis();
-  //Wait for incoming bytes or exit if timeout
-  while ((Serial.available() < num_bytes) && (millis() - startTime < timeout)){}
-}
-
-void read_color(int8_t* buffer) {
-  wait_for_bytes(3, 100); // Wait for 2 bytes with a timeout of 100 ms
-  read_signed_bytes(buffer, 3);
-}
-
-//This is used when I want to get multiple bytes from the stream.
-void read_signed_bytes(int8_t* buffer, size_t n){
-  size_t i = 0;
-  int c;
-  while (i < n){
-    c = Serial.read();
-	if (c < 0) break;
-    *buffer++ = (int8_t) c;
-    i++;
-  }
 }
 
 //We'll eventually use this to read desired motor speed.
